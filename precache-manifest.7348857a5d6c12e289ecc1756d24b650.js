7182c4ea2d136792b123ba54d5f69221cf0e4769
LeFizzy <contact@milanzeisler.hu>
2021-03-31 18:34:13 +0200

    Updates

---
+++ precache-manifest.7348857a5d6c12e289ecc1756d24b650.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8824f458e1876f151cda0511d21d36d6",
+    "url": "./index.html"
+  },
+  {
+    "revision": "3b8043b34756afad6cb8",
+    "url": "./static/css/main.9229b2f7.chunk.css"
+  },
+  {
+    "revision": "e9e3a15632b975625333",
+    "url": "./static/js/2.6f783d5d.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.6f783d5d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3b8043b34756afad6cb8",
+    "url": "./static/js/main.7898d810.chunk.js"
+  },
+  {
+    "revision": "5972a5cda2d6ba7153a9",
+    "url": "./static/js/runtime-main.6ba57da2.js"
+  }
+]);
